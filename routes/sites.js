1501ae6ec5a04d661385b76b3f14ee28ff4f5793
Yagi Anggar Prahara <yagi@wego.com>
2013-12-15 01:53:04 +0700

    Added new routes

--- routes/sites.js
+++ routes/sites.js
@@ -1,3 +1,7 @@
 exports.site_list = function(req, res){
   res.render('your_sites', { title: 'Your Sites', msg : 'This is site page' });
 };
+
+exports.add = function(req, res){
+  res.render('add_sites', { title: 'Add New Sites', msg : 'This is site page' });
+};
